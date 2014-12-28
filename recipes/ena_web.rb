6dedf5beb8c210feb69df724a6ee846a010056a9
SHIOYA, Hiromu <kwappa.856@gmail.com>
2014-12-29 08:11:57 +0900

    write recipe to install nginx
    * register yum repository
    * install nginx

--- recipes/ena_web.rb
+++ recipes/ena_web.rb
@@ -1,3 +1,7 @@
 # nginx
-service :nginx
-package :nginx
+execute 'resister yum repository for nginx' do
+  command 'rpm -ivh http://nginx.org/packages/centos/6/noarch/RPMS/nginx-release-centos-6-0.el6.ngx.noarch.rpm'
+end
+
+service 'nginx'
+package 'nginx'
