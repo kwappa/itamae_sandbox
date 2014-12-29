59d952d7f695020622b2474f7f9ef528a2b14d72
SHIOYA, Hiromu <kwappa.856@gmail.com>
2014-12-29 11:01:30 +0900

    edit iptables to listen 80 / 443

--- recipes/ena_web.rb
+++ recipes/ena_web.rb
@@ -1,3 +1,9 @@
+# open HTTP / HTTPS
+execute '/sbin/iptables -I INPUT 5 -p tcp --dport https -j ACCEPT'
+execute '/sbin/iptables -I INPUT 5 -p tcp --dport http -j ACCEPT'
+execute '/sbin/service iptables save'
+execute '/etc/init.d/iptables restart'
+
 # nginx
 execute 'resister yum repository for nginx' do
   command 'rpm -ivh http://nginx.org/packages/centos/6/noarch/RPMS/nginx-release-centos-6-0.el6.ngx.noarch.rpm'
